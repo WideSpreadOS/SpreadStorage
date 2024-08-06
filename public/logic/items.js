f61ca75fe74599eed6bf6e5790a378e29930f3d9
oLabs <jonny.olabs@gmail.com>
2024-08-06 14:33:04 -0400

    added new item list logic and items list in items.js

---
+++ public/logic/items.js
@@ -0,0 +1,87 @@
+export const items = [
+    {
+    id: 'jkjk19861776',
+    qr: 'asdf-asdf-asdf',
+    location: '1a1a1a',
+    route: 'storage/u12345678/1/a/1/a/1/a',
+    name: 'Thing 1',
+    description: 'This is something I found in 1994 in Millheim, PA.',
+    notes: [
+        'DO NOT THROW OUT'
+    ],
+    images: [
+        './public/media/image/item/1.png'
+    ],
+    fragile: false,
+    materials: ['oak wood', 'stainless steel', 'clear plastic'],
+    size: {
+        w: 25,
+        h: 55,
+        d: 25
+    },
+    weight: 8.5,
+    relations: [
+        'jkjk19861775',
+        'jkjk19861777',
+        'jkjk19861778',
+        'jkjk19861779',
+        'jkjk19861780'
+    ]
+},
+
+{
+    id: 'jkjk19861777',
+    qr: 'asdf-asdf-fdsa',
+    location: '1a1a1b',
+    route: 'storage/u12345678/1/a/1/a/1/b',
+    name: 'Thing 3',
+    description: 'This is something else I found in 1994 in Millheim, PA.',
+    notes: [
+        'DO NOT THROW OUT'
+    ],
+    images: [
+        './public/media/image/item/1.png'
+    ],
+    fragile: true,
+    materials: ['stainless steel', 'clear plastic'],
+    size: {
+        w: 5,
+        h: 15,
+        d: 5
+    },
+    weight: 1.5,
+    relations: [
+        'jkjk19861775',
+        'jkjk19861776',
+        'jkjk19861778',
+        'jkjk19861779',
+        'jkjk19861780'
+    ]
+},
+
+{
+    id: 'jkjk11061777',
+    qr: 'agdf-asdf-fdsa',
+    location: '1a1a1c',
+    route: 'storage/u12345678/1/a/1/a/1/c',
+    name: 'Thing 2',
+    description: 'This is something I found in 1998 in Cromwell, CT.',
+    notes: [
+        'DO NOT THROW OUT'
+    ],
+    images: [
+        './public/media/image/item/1.png'
+    ],
+    fragile: true,
+    materials: ['stainless steel', 'colored plastic'],
+    size: {
+        w: 12,
+        h: 8,
+        d: 4
+    },
+    weight: 1.85,
+    relations: [
+    ]
+},
+
+]
--- public/logic/main.js
+++ public/logic/main.js
@@ -1,9 +1,4 @@
-console.log("%c> main.js", "color: rgba(212, 7, 255, 1); font-size: 2rem; margin-top: 1rem; padding-left: 2rem;")
-
-
-
-
-export async function addItem(item) {
+export function addItem(item) {
     console.log("%cItem:", "color: rgba(7, 212, 255, 1); font-size: 1.5rem; margin-top: 1rem;")
     // console.table(item)
     console.log(item)
@@ -21,6 +16,8 @@ export async function addItem(item) {
         <div class="storage-item-label">
             <h3>${item.name}</h3>
             <h6>Physical Location: ${item.location} <span class="${itemFragileClass}">Fragile!</span></h6>
+
+            <img class="item-image" src="${item.images[0]}" alt="Image of ${item.name}">
             <p>${item.description}</p>
         </div>
     `
@@ -30,8 +27,3 @@ export async function addItem(item) {
     labelEl.innerHTML = formattedLabel
     spreadBody.appendChild(labelEl)
 }
-
-
-
-
-// console.log(item1)
